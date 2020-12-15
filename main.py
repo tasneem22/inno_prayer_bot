c7fc1365b807213e737103a479e245295cca254f
tasneem <tasneemsherif23@gmail.com>
2020-12-15 12:31:56 +0300

    changed to webhook + Pocfile

--- main.py
+++ main.py
@@ -80,13 +80,13 @@ updater.idle()
 import os
 import logging
 import gspread
-import telegram
 
 from oauth2client.service_account import ServiceAccountCredentials
 from telegram.ext import Updater, CommandHandler, MessageHandler,    Filters, InlineQueryHandler, Job, JobQueue,MessageHandler
 from dotenv import load_dotenv
 from datetime import datetime,time
-from array import *
+from pytz import timezone
+
 
 # parsing data first!!, into a 2D array
 scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
@@ -99,7 +99,8 @@ sheet_data = sheet.get_all_values()
 Prayer_name = sheet_data[0]
 Prayer_status = sheet_data[1]
 
-
+#set the port number to listen in for the webhook.
+PORT = int(os.environ.get('PORT', 5000))
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
@@ -109,9 +110,7 @@ TOKEN = os.getenv('BOT_TOKEN')
 updater = Updater(token=TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 j = updater.job_queue
-
-
-
+moscow = timezone('Europe/Moscow')
 
 def callback_alarm(context):
     context.bot.send_message(chat_id=(context.job.context[0]),
@@ -126,7 +125,7 @@ def callback_timer(context):
         current_day = now.day
         current_array = sheet_data[current_day + 1]
         current_time = time(int(current_array[x].split(':')[0]) , int(current_array[x].split(':')[1]))
-        j.run_once(callback_alarm, current_time , context=[context.job.context[1],x])
+        j.run_once(callback_alarm, current_time,tzinfo=moscow , context=[context.job.context[1],x])
 
 
 
@@ -139,7 +138,7 @@ def start(update, context):
         x = i+40
         if x > sheet.row_count:
             break
-        j.run_daily(callback_timer,time(13,55),context=sheet.row_values(x))
+        j.run_daily(callback_timer,time(16,16),context=sheet.row_values(x))
 
 
 
@@ -150,7 +149,12 @@ dispatcher.add_handler(start_handler)
 print(updater.bot)
 
 
-updater.start_polling()
+#updater.start_polling()
+updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+updater.bot.setWebhook('https://prayer-time-bot.herokuapp.com/' + TOKEN)
+
 updater.idle()
 <<<<<<< HEAD
 >>>>>>> b13c850... Initial commit
